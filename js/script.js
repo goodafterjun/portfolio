c07e893b2d783f282d1647727e783f01554e568f
Christian Scillitoe <christian.scillitoe@gmail.com>
2023-04-30 09:07:10 -0700

    build website

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+$(document).ready(function() {
+
+	$("#typed").typed({
+		strings: ["Hi, I’m Jun!"],
+		typeSpeed: 100,
+		startDelay: 0,
+		backSpeed: 60,
+		backDelay: 1000,
+		loop: true,
+		contentType: 'html'
+	});
+
+	// OFI Browser
+	objectFitImages();
+});
